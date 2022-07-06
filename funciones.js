a3c40447fa00b453c6ea25d2370226be4a1b2db3
tonybalde <tonybalde@hotmail.com>
2022-07-06 17:46:32 -0300

    correcion de funcion id pares

--- funciones.js
+++ funciones.js
@@ -1,6 +1,6 @@
 //  Crea un archivo JS que contenga las siguientes líneas
 
-// - Una función sin parámetros que devuelva siempre "true"
+// - Una función sin parámetros que devuelva siempre "true" - OK
 
 function myFunction(){
     return true;
@@ -8,24 +8,24 @@ function myFunction(){
 
 console.log(myFunction());
 
-// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado - OK
 
 setTimeout(function(){
     console.log("Hola soy una promesa")
 }, 5000)
+
+////////////////  otra forma de hacerlo es: 
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
 
 // - Una función generadora de índices pares automáticos
 
 function* generarId(){
     let id = 0;
     while (true){
-        id += 2
-        if (id === 10){
-            return id
-        }
-    }   yield id
+        yield id += 2
+    }  
 }
 
-const gen = generarId();
-
-console.log(gen.next().value);
